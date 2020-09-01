fec7e760f5a69aaece57233ee82b82fdb28128f4
rooat <welling1234@gmail.com>
2020-09-01 15:12:07 +0800

    u

--- test/changeAddress.js
+++ test/changeAddress.js
@@ -10,7 +10,7 @@ var Witness = mongoose.model('Witness');
 var TokenTransfer = mongoose.model('TokenTransfer');
 
 
-async function calTrans(){
+async function changeAddr(){
     let txList = await Address.find().sort({"balance":-1})
     if(txList && txList.length>0){
         for(var i=0;i<txList.length;i++){
@@ -24,4 +24,19 @@ async function calTrans(){
     }
     
 }
-calTrans();
+// changeAddr();
+async function changeBlock(){
+    let txList = await Block.find()
+    if(txList && txList.length>0){
+        for(var i=0;i<txList.length;i++){
+            if(txList[i].miner.substr(0,2)=="0x"){
+                console.log("addr--",txList[i].miner)
+                let new_addr ="tit"+txList[i].miner.substr(2);
+                await Block.update({"_id":txList[i]._id},{$set:{"addr":new_addr}})
+            }
+        }
+        console.log("ok---")
+    }
+    
+}
+changeBlock();
